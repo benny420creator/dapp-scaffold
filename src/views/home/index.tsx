7a6d4cf74c8dfe5dda9a7cca353356f1eb4c32b3
benny420creator <118639969+benny420creator@users.noreply.github.com>
2024-10-04 12:26:25 -0700

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -34,10 +34,15 @@ export const HomeView: FC = ({ }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <div className='mt-6'>
-        window.Jupiter.init({
+    const MyComponent = () => {
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.Jupiter) {
+      window.Jupiter.init({
         displayMode: "widget",
         endpoint: "https://api.mainnet-beta.solana.com",
-});
+      });
+    }
+  }, []);
         <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
         <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
           Solana Next
